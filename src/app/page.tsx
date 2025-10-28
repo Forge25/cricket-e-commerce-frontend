15ec3122e8e61a2d4ec9d7cc856c806cabdeb9de
ssulochanaherath <ssulochanaherath@gmail.com>
2025-10-28 20:41:13 +0530

    fix issue in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,9 +81,9 @@ export default function AllBatsPage() {
 
             {/* Breadcrumb */}
             <p className="text-sm text-black mb-2 flex items-center gap-1">
-              <link href="/" className="flex items-center gap-1 hover:underline text-black font-medium">
+              <a href="/" className="flex items-center gap-1 hover:underline text-black font-medium">
                 <Home className="w-4 h-4" />
-              </link>
+              </a>
               <span>/</span>
               Collections
               <span>/</span>
