1833b7e71fb83ddbd2ec023d0307f801a26464f2
rashmikaz <phrashmika@gmail.com>
2025-11-03 23:07:16 +0530

    user verify implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Login from "./auth/login/page";
+import Verify from "./auth/verify/page";
 
 export default function Home() {
   return (
     <>
-      <Login />
+      <Verify />
     </>
   );
 }
