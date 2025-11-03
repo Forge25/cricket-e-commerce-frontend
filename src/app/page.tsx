8af0b4d7d5d271b711da2083edb7492163bae113
rashmikaz <phrashmika@gmail.com>
2025-11-03 22:51:51 +0530

    login page complete with responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import SignUp from "./auth/signup/page";
+import Login from "./auth/login/page";
 
 export default function Home() {
   return (
     <>
-      <SignUp />
+      <Login />
     </>
   );
 }
